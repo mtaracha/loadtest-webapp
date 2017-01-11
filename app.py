d2be6ef1f82ceb0c87366b798f2d35672d666a4f
mtaracha <marcintaracha1991@gmail.com>
2017-01-11 00:05:34 +0000

    Create /loadtest endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import requests
 import time
 from datetime import datetime
@@ -20,11 +21,13 @@ q = Queue(connection=conn)
 
 from models import *
 
-def processor_time():
-	print "Blah blah"
+def processor_heavy_operation():
+
+	subprocess.call(["dd", "if=/dev/zero", "of=testfilefast", "bs=1024", "count=100240"])
+	#dd if=/dev/random of=bigfile bs=1024 count=102400
+	#subprocess.call(["dd", "if=/dev/urandom", "of=testfileslow", "bs=64M", "count=16"])
 	return 69 
 
-#def count_and_save_words(url):
 def ping_and_save_url(url):
     errors = []
 
@@ -35,14 +38,19 @@ def ping_and_save_url(url):
             "Unable to get URL. Please make sure it's valid and try again."
         )
         return {"error": errors}
+
+    start_time = time.time()
+    processor_heavy_operation()
+    #end_time = #
+    operation_time = time.time() - start_time
     
     # save the results
     try:
     	from models import Result
         result = Result(
             url=url,
-            operation_time = "1s",
-            operation_string= "ls -la", 
+            operation_time = operation_time,
+            operation_string= "dd if=/dev/random of=bigfile bs=1024 count=102400", 
             start_date = datetime.utcnow()          
         )
         db.session.add(result)
@@ -52,6 +60,23 @@ def ping_and_save_url(url):
         errors.append("Unable to add item to database.")
         return {"error": errors}
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    results = {}
+    if request.method == "POST":
+        # get url that the person has entered
+        url = request.form['url']
+        if 'http://' not in url[:7]:
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=ping_and_save_url, args=(url,), result_ttl=5000
+        )
+        print(job.get_id())
+        print(url)
+
+
+    return render_template('index.html', results=results)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
